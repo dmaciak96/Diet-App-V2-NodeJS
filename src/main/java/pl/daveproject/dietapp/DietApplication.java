7e644f1e0f41a5539f30982e37abdebc9a6335a6
Dawid <maciak.dawid.96@gmail.com>
2024-03-19 12:03:23 +0100

    Implement Virtual threads configuration

--- src/main/java/pl/daveproject/dietapp/DietApplication.java
+++ src/main/java/pl/daveproject/dietapp/DietApplication.java
@@ -4,9 +4,11 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 @PWA(name = "Webdiet", shortName = "Webdiet")
 public class DietApplication implements AppShellConfigurator {
